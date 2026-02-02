724889451c929a76f0e50d80293b293c6e70990d
Freddy Alvarado Rodríguez <falvarador@Freddys-MacBook-Pro.local>
2026-02-02 10:36:27 -0600

    Remove nuxt frontend

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///Users/falvarador/Development/Projects/2buylist/frontend/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
